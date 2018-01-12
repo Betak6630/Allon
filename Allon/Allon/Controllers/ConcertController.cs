4cd62416c6d2dba8f6f9a886874b81502fa9366e
Betak6630 <Betak6630@mail.ru>
2018-01-12 19:34:26 +0300

    add page Concert

---
+++ Allon/Allon/Controllers/ConcertController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace Allon.Controllers
+{
+    public class ConcertController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Phone()
+        {
+            return View();
+        }
+    }
+}
